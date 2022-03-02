bb9ae0f6eda63488e3780bb002be73367d082e9a
Nicholas Nikas <nicholasnikas@Nicholass-MacBook-Pro.local>
2022-03-02 00:34:02 -0500

    Profile Screen & Get User Details

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -30,7 +30,9 @@ const Header = () => {
               </LinkContainer>
               {userInfo ? (
                 <NavDropdown title={userInfo.name} id='username'>
-                  <NavDropdown.Item>Profile</NavDropdown.Item>
+                  <LinkContainer to='/profile'>
+                    <NavDropdown.Item>Profile</NavDropdown.Item>
+                  </LinkContainer>
                   <NavDropdown.Item onClick={logoutHandler}>
                     Logout
                   </NavDropdown.Item>
